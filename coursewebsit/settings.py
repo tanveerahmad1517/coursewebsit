6aad7593f2c011bf65cfa538dcece3899df54e48
tanveerahmad1517 <tanveerobjects@gmail.com>
2018-04-11 19:04:42 +0500

    changed again

--- coursewebsit/settings.py
+++ coursewebsit/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new!
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
