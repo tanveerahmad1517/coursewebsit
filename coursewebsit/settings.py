6eaaeff8a4eb6ec15dfe6824fcf41c85a7b0d2e9
tanveerahmad1517 <tanveerobjects@gmail.com>
2018-04-11 18:18:20 +0500

    modified

--- coursewebsit/settings.py
+++ coursewebsit/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -39,10 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'courses',
     'django.contrib.humanize',
-    'bootstrap3',
     'widget_tweaks',
-    'materializecssform',
-    'material',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +127,3 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'assets', 'media')
 MEDIA_URL = '/media/'
-
-LOGIN_REDIRECT_URL = '/courses'
-django_heroku.settings(locals())
